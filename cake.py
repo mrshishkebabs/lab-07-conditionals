33ecfc9a4c6e662483493ed40ef1605684d5c1a6
Ken Park <xkenxparkx@gmail.com>
2021-10-25 16:21:04 -0400

    Added MORE files on Spanish

--- cake.py
+++ cake.py
@@ -4,5 +4,5 @@ if answer == "yes" or answer == "Yes" or answer == "y" or answer == "Y":
 elif answer == "no" or answer == "No" or answer == "n" or answer == "N":
 	print("No cake for you.")
 else: 
-	print("ERROR: Sorry, I don't understand!");
+	print("ERROR: Sorry, I don't understand!")
 
---
+++ spanish.py
@@ -0,0 +1,9 @@
+﻿spanish = input("Give me a word in English...")
+if spanish == "Cat" or spanish == "cat":
+	print("gato")
+elif spanish == "Dog" or spanish == "dog":
+	print("perro")
+elif spanish == "Horse" or spanish == "dog":
+	print("caballo")
+else: 
+	print("no entiendo")
