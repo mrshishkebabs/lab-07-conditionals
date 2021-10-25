30044c4b0affc33e5d4b7332c0ddec6c6728f5d0
Ken Park <xkenxparkx@gmail.com>
2021-10-25 16:01:12 -0400

    Added the CAKE file for Python and HTML

---
+++ cake.py
@@ -0,0 +1,8 @@
+﻿answer = input("Do you want cake? (Yes/No) ")
+if answer == "yes" or answer == "Yes" or answer == "y" or answer == "Y":
+	print("Have some cake.")
+elif answer == "no" or answer == "No" or answer == "n" or answer == "N":
+	print("No cake for you.")
+else: 
+	print("ERROR: Sorry, I don't understand!");
+
